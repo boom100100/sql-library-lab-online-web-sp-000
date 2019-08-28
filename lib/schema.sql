a8a1256e47f31a63fe5c6661e3c98f15c4fe22f6
Bernadette Davis <bsd231@nyu.edu>
2019-08-28 16:08:30 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -5,6 +5,14 @@ CREATE TABLE series(
   subgenre_id INTEGER
 );
 
+CREATE TABLE characters(
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  species TEXT,
+  motto TEXT,
+  author_id INTEGER
+);
+
 CREATE TABLE subgenres(
   id INTEGER PRIMARY KEY,
   name TEXT
@@ -22,13 +30,7 @@ CREATE TABLE books(
   series_id INTEGER
 );
 
-CREATE TABLE characters(
-  id INTEGER PRIMARY KEY,
-  name TEXT,
-  species TEXT,
-  motto TEXT,
-  author_id INTEGER
-);
+
 
 CREATE TABLE character_books(
   id INTEGER PRIMARY KEY,
